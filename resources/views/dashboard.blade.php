cf0298c42df9d81fc5d04369a28beeeebbdfcaf9
lucianor <regueraluciano@gmail.com>
2022-10-26 20:56:27 -0300

    Vista muro finalizada

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,26 @@
 @extends('layouts.app')
 
 @section('title')
-    Tu Cuenta
+    Perfil: {{ $user->username }}
 @endsection
 
 @section('content')
     <div class="flex justify-center">
-        <div class="w-full md:w-8/12 lg:w-6/12 md:flex">
-            <div class="md:w-8/12 lg:w-6/12 px-5">
+        <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col md:flex-row items-center">
+            <div class="w-8/12 lg:w-6/12 px-5">
                 <img src="{{ asset('img/usuario.svg') }}" alt="Imagen usuario">
             </div>
-            <div class="md:w-8/12 lg:w-6/12 px-5">
-                <p class="text-gray-700 text-2xl">{{ auth()->user()->name }}</p>
+            <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py:10 md:py-10">
+                <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
+                <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
+                    0 <span class="font-normal"> Seguidores</span>
+                </p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0 <span class="font-normal"> Siguiendo</span>
+                </p>
+                <p class="text-gray-800 text-sm mb-3 font-bold">
+                    0 <span class="font-normal"> Posts</span>
+                </p>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,4 @@ Route::post('/login', [LoginController::class, 'store']);
 
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
-Route::get('/muro', [PostController::class, 'index'])->name('posts.index');
+Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
