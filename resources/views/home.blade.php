25577bb81be7cfc51761943e4b2cb64016525882
lucianoreguera <regueraluciano@gmail.com>
2022-10-26 13:32:10 -0300

    Modulo usuarios finalizado

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title')
+    Página Principal
+@endsection
+
+@section('content')
+    Contenido de la página
+@endsection
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title')
+    Muro
+@endsection
+
+@section('content')
+    <p>Bienvenidos al muro</p>
+@endsection
